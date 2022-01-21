d280e6f626ffeebf73a4e01b647dfd8da21b3cea
TheReaper62 <34223146+TheReaper62@users.noreply.github.com>
2022-01-21 21:35:01 +0800

    Merge Async with Basic

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from distutils.core import setup
 setup(
     name='subapy',
     packages=['subapy'],
-    version='0.1',
+    version='0.3',
     license='MIT',
     description='Supabase API Wrapper',
     author='FishballNoodles',
     author_email='joelkhorxw@gmail.com',
     url='https://github.com/TheReaper62/Subapy',
-    download_url='https://github.com/TheReaper62/Subapy/archive/refs/tags/v0.1.tar.gz',
-    keywords=['supabase', 'api', 'wrapper'],
+    download_url='https://github.com/TheReaper62/Subapy/archive/refs/tags/v0.3.tar.gz',
+    keywords=['supabase', 'api', 'wrapper','database','db'],
     install_requires=[
         'requests',
+        'asyncio'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
